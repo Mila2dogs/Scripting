26320245ea797ae9cd6f63d3b7344b288e39f164
Mila2dogs <mdanila06012014@gmail.com>
2025-07-24 11:37:51 +0300

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    public float speed;
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    void Update()
+    {
+        transform.position +=transform.forward * speed * Time.deltaTime;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        //Враг уничтожается
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+
+        //Снаряд уничтожается
+        Destroy(gameObject);
+
+    }
+}
